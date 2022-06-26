da3637ec42ab3b9c21153658a36f68d4a1f755e2
Anna <an.kunitskaya@gmail.com>
2022-06-26 22:24:43 +0300

    task811+test

--- src/main/java/Lesson_8/HW_81_task1.java
+++ src/main/java/Lesson_8/HW_81_task1.java
@@ -1,29 +1,33 @@
 package Lesson_8;
 
+import java.util.Locale;
+
 public class HW_81_task1 {
 /*
 * Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
-
 The output should be two capital letters with a dot separating them.
-
 It should look like this:
-
 Sam Harris => S.H
-
 patrick feeney => P.F*/
+
     String name;
 
-    String surname;
+
+    public String getName() {
+        return name;
+    }
 
     public HW_81_task1(String name) {
         this.name = name;
 
     }
-/*
+
 public static String abbrevName(String name) {
-    name = name.split(" ");
-    return name;
+    String[] arr = name.split(" ");
+    String result  = arr[0].charAt(0)+"."+arr[1].charAt(0);
+
+    return result.toUpperCase();
 }
-*/
+
 
 }
--- src/test/java/Lesson_8_Tests/HW_81_task1_Test.java
+++ src/test/java/Lesson_8_Tests/HW_81_task1_Test.java
@@ -1,27 +1,23 @@
 package Lesson_8_Tests;
 
 import Lesson_8.HW_81_task1;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
-import java.util.Arrays;
+
 
 public class HW_81_task1_Test {
 
-    public static void main(String[] args) {
+ @Test
+     public void abbrevNameTest(){
+     Assert.assertEquals(HW_81_task1.abbrevName("Anna Smith"), "A.S");
+     Assert.assertEquals(HW_81_task1.abbrevName("ella vita"), "E.V");
+        }
 
-
-        HW_81_task1 Anna = new HW_81_task1("Anna Smetankina");
-
-      String result = "Anna Smetankina";
-
-     //   String result = result.split(" ");
-
-
-
-
-        System.out.println(result);
-
-    }
+        }
 
 
 
-}
+
+
+
